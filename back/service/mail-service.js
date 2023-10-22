326202f0a87d89ae4bf02a08aac6e9a71e00d5ad
Kunerkhan Akerov <kakerov@nurtelecom.kg>
2023-10-22 23:47:32 +0600

    feat: implement activation link

--- back/service/mail-service.js
+++ back/service/mail-service.js
@@ -1,6 +1,37 @@
+const nodemailer = require("nodemailer");
+
 class MailService {
-    async sendActivationMail(to, lonk) {
-        
+    constructor() {
+        this.transporter = nodemailer.createTransport({
+            host: process.env.SMTP_HOST,
+            port: process.env.SMTP_PORT,
+            secure: false,
+            auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASSWORD
+            }
+        })
+    }
+
+    async sendActivationMail(to, link) {
+        try {
+            await this.transporter.sendMail({
+                from: process.env.SMTP_USER,
+                to,
+                subject: "Account activation on" + process.env.API_URL,
+                text: "",
+                html:
+                    `
+                    <div>
+                        <h1>For account activation use the link</h1>
+                        <a href="${link}">${link}</a>
+                    </div>
+                    `
+            })
+        } catch(error) {
+            console.log(error);
+        }
+
     }
 }
 
